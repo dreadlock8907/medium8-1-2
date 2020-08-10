b4080f1126560ac8a127008e49aee9f59e1042b2
dreadlockie <dreadlock8907@gmail.com>
2020-08-10 11:04:56 +0300

    fixes for code review

--- medium8-1-2.cs
+++ medium8-1-2.cs
@@ -7,29 +7,29 @@ namespace Task
     {
         public static void Main(string[] args)
         {
-            GameObjectsPool pool = new GameObjectsPool();
-            pool.Add(new GameObject(name: "1", isAlive: true, position: new Position(5, 5)));
-            pool.Add(new GameObject(name: "2", isAlive: true, position: new Position(10, 10)));
-            pool.Add(new GameObject(name: "3", isAlive: true, position: new Position(15, 15)));
+            TrajectorySimulator simulator = new TrajectorySimulator();
+            simulator.Add(new GameObject(name: "1", position: new Position(5, 5)));
+            simulator.Add(new GameObject(name: "2", position: new Position(10, 10)));
+            simulator.Add(new GameObject(name: "3", position: new Position(15, 15)));
 
             while(true)
             {
-                pool.DestroyAllCollided();
-                pool.MoveAllRandom();
-                pool.ShowAllAlive();
+                simulator.DestroyAllCollided();
+                simulator.MoveAllRandom();
+                simulator.ShowAllAlive();
             }
         }
     }
 
-    public class GameObjectsPool
+    public class TrajectorySimulator
     {
         readonly List<GameObject> _pool;
-        readonly Random random;
+        readonly Random _random;
 
-        public GameObjectsPool()
+        public TrajectorySimulator()
         {
             _pool = new List<GameObject>();
-            random = new Random();
+            _random = new Random();
         }
 
         public void Add(GameObject gameObject)
@@ -39,6 +39,14 @@ namespace Task
 
         public void DestroyAllCollided()
         {
+            List<GameObject> collided = FindAllCollided();
+            for (int i = 0; i < collided.Count; i++)
+                _pool.Remove(collided[i]);
+        }
+
+        private List<GameObject> FindAllCollided()
+        {
+            List<GameObject> collided = new List<GameObject>();
             for(int i = 0; i < _pool.Count; i++)
             {
                 for (int k = 0; k < _pool.Count; k++)
@@ -49,54 +57,50 @@ namespace Task
                     GameObject current = _pool[i];
                     GameObject other = _pool[k];
                     
+                    if(collided.Contains(current))
+                        continue;
+                    
                     if(current.IsCollidedWith(other)) 
-                        current.Destroy();
+                        collided.Add(current);
                 }
             }
+
+            return collided;
         }
 
         public void MoveAllRandom()
         {
             for(int i = 0; i < _pool.Count; i++)
-                _pool[i].MoveOn(random.Next(-1, 1), random.Next(-1, 1));
+                _pool[i].MoveOn(_random.Next(-1, 1), _random.Next(-1, 1));
         }
 
         public void ShowAllAlive()
         {
             for(int i = 0; i < _pool.Count; i++)
-            {
-                GameObject gameObject = _pool[i];
-                if(gameObject.IsAlive)
-                    gameObject.Show();
-            }
+                _pool[i].Show();
         }
     }
 
     public class GameObject
     {
         public Position Position { get; private set; }
-        public bool IsAlive { get; private set; }
-        string Name { get; set; }
+        private string Name { get; set; }
 
-        public GameObject(string name, bool isAlive, Position position)
+        public GameObject(string name, Position position)
         {
             Name = name;
-            IsAlive = isAlive;
             Position = position;
-        }
-        
-        public void Destroy()
-        {
-            IsAlive = false;
         }
 
         public void MoveOn(int x, int y)
         {
             int newX = Position.X + x;
             int newY = Position.Y + y;
+
+            if (newX < 0) newX = 0;
+            if (newY < 0) newY = 0;
             
-            Position.Set(newX, newY);
-            Position.Normalize();
+            Position = new Position(newX, newY);
         }
         
         public void Show()
@@ -111,45 +115,20 @@ namespace Task
         }
     }
 
-    public class Position
+    public struct Position
     {
-        public int X => _x.Value;
-        public int Y => _y.Value;
-        
-        Coordinate _x;
-        Coordinate _y;
+        public readonly int X;
+        public readonly int Y;
 
         public Position(int x, int y)
         {
-            _x = new Coordinate(x); 
-            _y = new Coordinate(y);
-        }
-
-        public void Set(int x, int y)
-        {
-            _x.Value = x;
-            _y.Value = y;
-        }
-
-        public void Normalize()
-        {
-            _x.Value = _x.Value > 0 ? _x.Value : 0;
-            _y.Value = _y.Value > 0 ? _y.Value : 0;
+            X = x;
+            Y = y;
         }
 
         public bool EqualsTo(Position position)
         {
             return X == position.X && Y == position.Y;
-        }
-    }
-
-    public class Coordinate
-    {
-        public int Value { get; set; }
-
-        public Coordinate(int coordinate)
-        {
-            Value = coordinate;
         }
     }
 }
